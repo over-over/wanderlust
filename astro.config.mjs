4ac061b81e8f5370b249a0c27df2d50cce87f30b
Vladislav Panov <vp@Vladislavs-MacBook-Air.local>
2024-05-29 23:01:48 +0200

    feat: base

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,8 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://over-over.github.io/',
-  base: 'wanderlust',
+  site: 'https://over-over.github.io',
+  base: '/wanderlust',
   output: 'static',
   outDir: './docs',
   integrations: [react()],
